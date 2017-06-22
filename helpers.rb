4a021e1077b090663c4969a7539dfb3280e92a99
Joseph Buchma <josephbuchma@gmail.com>
2017-06-22 19:36:38 +0300

    x top-row-repeats - look for token line-by-line repeats in files

--- helpers.rb
+++ helpers.rb
@@ -132,6 +132,67 @@ module Helpers
     end
   end
 
+  class X < Thor
+    desc 'top-row-repeats <token> [path]', 'this script is looking for line-by-line repetition of token and sorts results by max repetitions in a row'
+    option :max_distance, :type=>:numeric, :default=>0, :desc => 'set how many lines is acceptable between repeats'
+    option :filter_min, :type=>:numeric, :default=>0, :desc => 'filter out matches with less than filter_min matches in a row'
+    option :cnt, :type=>:boolean, :default=>false, :desc => 'show matches count (filename:line - count)'
+    option :vim, :type=>:boolean, :default=>false, :desc=>'open matched files in vim'
+    def top_row_repeats(token, path='.')
+      max_distance = options[:max_distance] || 0
+      filter_min = options[:filter_min] || 0
+      edit = options[:vim] || false
+      files = []
+      if File.file? path
+        files = [file]
+      else
+        path = File.join(path, '*') if File.directory? path
+        files = Dir[path].select{ |p| File.file? p }.uniq
+      end
+      matches = []
+      clean_match = lambda { {cnt: 0, file: nil, line: nil, offset: nil, text: nil} }
+      match = clean_match.call
+      missCnt = 0
+      files.each do |p|
+        File.foreach(p).with_index do |l, i|
+          if offset = l.index(token)
+            match[:file] ||= p
+            match[:line] ||= i+1
+            match[:offset] ||= offset
+            match[:text] ||= l.strip
+            match[:cnt] +=1
+          else
+            if missCnt < max_distance
+              missCnt += 1
+            else
+              if match[:cnt] > filter_min
+                matches << match
+              end
+              match = clean_match.call
+              missCnt = 0
+            end
+          end
+        end
+      end
+      if edit
+        fmt = lambda { |m| "#{m[:file]}:#{m[:line]}:#{m[:offset]} #{m[:text]} # (#{m[:cnt]} matches)" }
+      else
+        if options[:cnt]
+          fmt = lambda { |m| "#{m[:file]}:#{m[:line]} - #{m[:cnt]}" }
+        else
+          fmt =  lambda { |m| "#{m[:file]}:#{m[:line]}" }
+        end
+      end
+      lst = matches.sort{ |x,y| y[:cnt]<=>x[:cnt] }.map{ |m| fmt.call m }.join("\n")
+      if edit
+        File.write("/tmp/top_row_repeats_shelp_tmp.txt", lst)
+        Kernel.exec "vim -c 'cfile /tmp/top_row_repeats_shelp_tmp.txt | copen'"
+      else
+        puts lst
+      end
+    end
+  end
+
   class Helpers < Thor
     desc 'git SUBCOMMAND ...ARGS', 'git helpers'
     subcommand 'git', Git
@@ -144,6 +205,9 @@ module Helpers
 
     desc 'go SUBCOMMAND ...ARGS', 'golang tools'
     subcommand 'go', Go
+
+    desc 'x SUBCOMMAND ...ARGS', 'miscellanous'
+    subcommand 'x', X
 
     desc 'edit', 'edit this helpers'
     def edit
